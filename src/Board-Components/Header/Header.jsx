f2531adba0cbaa1d98805536e5f8a4dc10af69e5
Denis Mihai <birtalanmihaidenis@gmail.com>
2026-01-07 02:49:12 +0200

    banner

--- src/Board-Components/Header/Header.jsx
+++ src/Board-Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Header.css";
 import {
   MagnifyingGlassIcon,
@@ -8,9 +8,14 @@ import {
 } from "@phosphor-icons/react";
 import Picture from "./default_picture.jpg";
 
+const unsplashFetchURL =
+  "https://api.unsplash.com/photos/random?count=10&client_id=OHIYPwO7PbAph1p5sa6rM3E1hK5LmG-I8IugGOMU-O0";
+
 const Banner = () => {
   const [searchValue, setSearchValue] = useState("");
   const [bgMenu, setBgMenu] = useState("");
+  const [photos, setPhotos] = useState([]);
+  const [background, setBackground] = useState(null);
 
   const handleSearch = (e) => {
     if (e.key == "Enter") {
@@ -23,8 +28,30 @@ const Banner = () => {
   };
 
   const handleButtonClick = () => {
-    setBgMenu((prev) => (prev == "" ? "home" : ""));
+    setBgMenu((prev) => (prev == "" ? "Home" : ""));
   };
+
+  const handleChangeBackground = (src) => {
+    setBackground(src);
+  };
+
+  const fetchPhotos = async () => {
+    const res = await fetch(
+      "https://api.pexels.com/v1/curated?per_page=100&query=landscape",
+      {
+        headers: {
+          Authorization:
+            "18GWqIrUwAJSFlXqDk0Cpnz7VleRTd5UvcMETL5dhW5JvMQX62QLr2G1",
+        },
+      }
+    );
+    const data = await res.json();
+    setPhotos(data.photos);
+  };
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
 
   return (
     <>
@@ -55,23 +82,48 @@ const Banner = () => {
             <SelectionBackgroundIcon />
           </div>
           {bgMenu && (
-            <div className="bgMenu_container">
+            <div
+              className={`bgMenu_container ${
+                bgMenu == "Pictures" ? "big" : ""
+              }`}
+            >
               <div className="bgMenu_header">
-                <div className="bgMenu_button">
-                  <ArrowBendUpLeftIcon />
-                </div>
-                <div className="bgMenu_title">Change Background</div>
-                <div className="bgMenu_button">
+                {bgMenu != "Home" && (
+                  <div
+                    className="bgMenu_button"
+                    onClick={() => {
+                      setBgMenu("Home");
+                    }}
+                  >
+                    <ArrowBendUpLeftIcon />
+                  </div>
+                )}
+                {bgMenu == "Home" && (
+                  <div className="bgMenu_button" style={{ opacity: 0 }}></div>
+                )}
+
+                {bgMenu == "Home" && (
+                  <div className="bgMenu_title">Change Background</div>
+                )}
+                {bgMenu != "Home" && (
+                  <div className="bgMenu_title">{bgMenu}</div>
+                )}
+                <div
+                  className="bgMenu_button"
+                  onClick={() => {
+                    setBgMenu("");
+                  }}
+                >
                   <XIcon />
                 </div>
               </div>
-              {bgMenu == "home" && (
+              {bgMenu == "Home" && (
                 <div className="bgMenu_home">
                   <div className="bgMenu_content">
                     <div
                       className="selection_container"
                       onClick={() => {
-                        setBgMenu("pictures");
+                        setBgMenu("Pictures");
                       }}
                     >
                       <div className="selection_image">
@@ -79,7 +131,12 @@ const Banner = () => {
                       </div>
                       <div className="selection_title">Pictures</div>
                     </div>
-                    <div className="selection_container">
+                    <div
+                      className="selection_container"
+                      onClick={() => {
+                        setBgMenu("Colors");
+                      }}
+                    >
                       <div className="selection_image"></div>
                       <div className="selection_title">Colors</div>
                     </div>
@@ -88,10 +145,37 @@ const Banner = () => {
                   <div className="bgMenu_custom"></div>
                 </div>
               )}
+              {bgMenu == "Pictures" && (
+                <div className="bgMenu_pictures">
+                  {photos.map((val) => (
+                    <div
+                      className="photo_image"
+                      key={val.id}
+                      onClick={() => handleChangeBackground(val.src.original)}
+                    >
+                      <img src={val.src.medium} alt="pula"></img>
+                    </div>
+                  ))}
+                </div>
+              )}
             </div>
           )}
         </div>
       </div>
+      <div
+        className="main-bg"
+        style={{
+          backgroundImage: `url(${background})`,
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          zIndex: -1,
+        }}
+      ></div>
     </>
   );
 };
